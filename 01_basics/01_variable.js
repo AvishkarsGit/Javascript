c11dcf2401cb9c95f04aa780735fdeb8d49790be
AvishkarGit <109093669+AvishkarsGit@users.noreply.github.com>
2024-01-08 12:35:35 +0000

    variables and datatypes

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,6 @@
+const accountId = 1010;
+
+let username = 'avishkar'
+var password = '12345'
+
+console.table([accountId,username,password]);   
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,7 @@
+let name = 'avishkar'
+let age = 19
+let isStudying = true
+
+console.table([typeof name,typeof age, typeof isStudying]);
+console.log(typeof null);
+console.log(typeof undefined);

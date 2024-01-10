abca746ceb2f0c18103b6a6c34d2ac1d8b55eae3
AvishkarGit <109093669+AvishkarsGit@users.noreply.github.com>
2024-01-10 13:33:17 +0000

    maths and number

---
+++ 01_basics/08_math_and_number.js
@@ -0,0 +1,13 @@
+
+// Math
+
+console.log(Math)
+console.log(Math.abs(-5))
+console.log(Math.round(10.65887));
+console.log(Math.floor(2.45))
+console.log(Math.floor(Math.sqrt(45)))
+
+// Number
+let num = 10000000000
+console.log(num)
+console.log(num.toLocaleString('en-IN'))

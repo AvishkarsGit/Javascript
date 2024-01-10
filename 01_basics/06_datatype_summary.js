1d2e206d9f7475fb6aa79847738e043714a9449f
AvishkarGit <109093669+AvishkarsGit@users.noreply.github.com>
2024-01-10 08:32:24 +0000

    datatype summary

--- 01_basics/06_datatype_summary.js
+++ 01_basics/06_datatype_summary.js
@@ -5,6 +5,13 @@ console.log('value=',age,'\ntype = ',typeof(age));
 let name = 'avishkar'
 console.log('value = ',name,'\ntype = ',typeof(name))
 
+const id = Symbol('123')
+console.log(id)
+const anotherId = Symbol('123')
+console.log(anotherId)
+
+console.log(id === anotherId)
+
 //non -primitive data type
 
 const myArray = ['avishkar','abhijeet','android']

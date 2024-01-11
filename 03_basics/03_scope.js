cf9a7999a3d84dd9311fb28a3be295df1012aa7d
AvishkarGit <109093669+AvishkarsGit@users.noreply.github.com>
2024-01-11 20:18:52 +0000

    scope

---
+++ 03_basics/03_scope.js
@@ -0,0 +1,13 @@
+function one(){
+    const username ='avishkar'
+
+    function two(){
+        const website ='https://www.udemy.com/'
+        console.log(username)
+    }
+    // console.log(website)
+
+    two()
+}
+
+one()   

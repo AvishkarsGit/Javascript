63ca4d073d1b69af0b2e83f227d7ae92335cf13d
Avishkar Kumbhar <kumbharavishkar7@gmail.com>
2024-01-14 17:37:33 +0530

    for-in loop

---
+++ 05_iterations/four.js
@@ -0,0 +1,18 @@
+const extensions = {
+    js :'javascript',
+    java :'java',
+    py : 'python',
+    cpp : 'c++',
+}
+
+//for in loop
+
+for (const key in extensions){
+    // console.log(`${key} shortcuts is for ${extensions[key]}`);
+}
+
+const programming = ['c','c++','java','javascript','python']
+for (const key in programming) {
+    console.log(programming[key]);
+}
+

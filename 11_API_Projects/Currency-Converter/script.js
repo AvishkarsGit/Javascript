9731329b5749aea864baa71f335c316bb49e309d
AvishkarsGit <kumbharavishkar7@gmail.com>
2024-11-07 20:40:59 +0530

    uncomment one line

--- 11_API_Projects/Currency-Converter/script.js
+++ 11_API_Projects/Currency-Converter/script.js
@@ -1,4 +1,4 @@
-//const BASE_URL = "https://v6.exchangerate-api.com/v6/fffffbb1221e4779300c80ce/latest/INR";
+const BASE_URL = "https://v6.exchangerate-api.com/v6/fffffbb1221e4779300c80ce/latest/INR";
 // const from = document.querySelector('#fromSelect');
 // const to = document.querySelector('#toSelect');
 

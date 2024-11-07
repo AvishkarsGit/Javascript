8cbd3a8fa600c07c397ed73dca71c61509c24f94
AvishkarsGit <kumbharavishkar7@gmail.com>
2024-11-08 01:20:16 +0530

    Currency converter app complete

--- 11_API_Projects/Currency-Converter/script.js
+++ 11_API_Projects/Currency-Converter/script.js
@@ -1,5 +1,50 @@
-const BASE_URL = "https://v6.exchangerate-api.com/v6/fffffbb1221e4779300c80ce/latest/INR";
-// const from = document.querySelector('#fromSelect');
-// const to = document.querySelector('#toSelect');
+let BASE_URL = "https://v6.exchangerate-api.com/v6/fffffbb1221e4779300c80ce/latest/USD";
+const from = document.querySelector('#fromSelect');
+const to = document.querySelector('#toSelect');
+let msg = document.querySelector('.msg');
+let convertBtn = document.querySelector('#btn');
+let amtVal = document.querySelector('form input');
 
-console.log('hello');
+
+function createDropDown(element) {
+    for (currency in countryList) {
+        let option = document.createElement('option');
+        option.value = currency;
+        option.text = currency;
+        element.add(option)
+    }
+}
+
+createDropDown(from);
+createDropDown(to)
+
+//setting default values
+from.value = 'USD';
+to.value = 'INR';
+
+let convertCurrency = async () => {
+    const amount = amtVal.value;
+
+    if (amount < 1) {
+        alert('amount must be greater than 0')
+    }
+    else {
+
+        let fromCurrency = from.value;
+        let toCurrency = to.value;
+        
+        let response = await fetch(BASE_URL);
+        let data = await response.json();
+        let fromRate = data.conversion_rates[fromCurrency];
+        let toRate = data.conversion_rates[toCurrency];
+        const finalAmt = (amount / fromRate) * toRate;
+    
+        msg.innerText = `${amount} ${fromCurrency} = ${finalAmt} ${toCurrency}`
+        msg.innerHTML = `${amount} <b>${fromCurrency}</b> = ${finalAmt} <b>${toCurrency}</b>`;
+    }
+    
+}
+
+
+convertBtn.addEventListener('click',convertCurrency);
+window.addEventListener('load',convertCurrency);

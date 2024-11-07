1e1bfa16b5885b7579aab961692bd30b1819957e
AvishkarGit <109093669+AvishkarsGit@users.noreply.github.com>
2024-11-07 15:04:05 +0000

    added new project folder;

---
+++ 11_API_Projects/Currency-Converter/script.js
@@ -0,0 +1,5 @@
+//const BASE_URL = "https://v6.exchangerate-api.com/v6/fffffbb1221e4779300c80ce/latest/INR";
+// const from = document.querySelector('#fromSelect');
+// const to = document.querySelector('#toSelect');
+
+console.log('hello');
